341edb2bfe00fc9e16f4b9200160608c0a29b50b
Sasaki <gu.sasaki2020@gmail.com>
2022-07-08 00:21:09 -0300

    criando injeção de dependencia

--- pep_8_typing/main.py
+++ pep_8_typing/main.py
@@ -1,4 +1,5 @@
 from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
 
 # fila_teste = filanormal()
 # fila_teste.atualizafila()
@@ -16,8 +17,9 @@ from fabrica_fila import FabricaFila
 # print(fila_teste_2.chama_cliente(2))
 # print(fila_teste_2.estatisticas('10/01/1993', 10, 'detail'))
 
-teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.estatistica(EstatisticaDetalhada('20/03/2005', 120)))

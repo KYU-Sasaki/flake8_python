99a60af133dadd08fd858f44f2e1923413a150f8
Sasaki <gu.sasaki2020@gmail.com>
2022-07-08 00:05:18 -0300

    criando mais paginas

--- pep_8_typing/main.py
+++ pep_8_typing/main.py
@@ -1,4 +1,4 @@
-from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
 # fila_teste = filanormal()
 # fila_teste.atualizafila()
@@ -8,10 +8,16 @@ from fila_prioritaria import FilaPrioritaria
 # print(fila_teste.chamacliente(10))
 # print(fila_teste.chamacliente(4))
 
-fila_teste_2 = FilaPrioritaria()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-print(fila_teste_2.chama_cliente(10))
-print(fila_teste_2.chama_cliente(2))
-print(fila_teste_2.estatisticas('10/01/1993', 10, 'detail'))
+# fila_teste_2 = FilaPrioritaria()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# print(fila_teste_2.chama_cliente(10))
+# print(fila_teste_2.chama_cliente(2))
+# print(fila_teste_2.estatisticas('10/01/1993', 10, 'detail'))
+
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
